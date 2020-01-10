1e64d114bbbff96e4e09940d40e38df168746204
Søren Lund <soren@lund.org>
2020-01-10 11:38:57 +0100

    Add cfn-mode

---
+++ config/contrib/cfn-mode.el
@@ -0,0 +1,74 @@
+;;; cfn-mode.el --- cloudformation modes -*-coding:utf-8-*-
+
+;; Copyright (c) 2019 Søren Lund <soren@lund.org>
+
+;; This file is part of dot-emacs.
+
+;; dot-emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; dot-emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with dot-emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Set up a modes for editiong AWS CloudFormation templates in either
+;; JSON or YAML format. The JSON mode is based on built-in js-mode and
+;; the YAML mode is based on yaml-mode if available.
+
+;; Furthermore, if flycheck is available the the external cfn-lint
+;; program is installed, the flycheck be configured to use cfn-lint
+;; for detecting problems with your templates.
+
+;;; Code:
+
+;; Set up a mode for JSON based templates
+(define-derived-mode cfn-json-mode js-mode
+    "CFN-JSON"
+    "Simple mode to edit CloudFormation template in JSON format."
+    (setq js-indent-level 2))
+
+(add-to-list 'magic-mode-alist
+             '("\\({[\n ]*\\)? *[\"']AWSTemplateFormatVersion" . cfn-json-mode))
+
+;; Set up a mode for YAML based templates if yaml-mode is installed
+(when (featurep 'yaml-mode)
+
+  (define-derived-mode cfn-yaml-mode yaml-mode
+    "CFN-YAML"
+    "Simple mode to edit CloudFormation template in YAML format.")
+
+  (add-to-list 'magic-mode-alist
+               '("\\(---\n\\)?AWSTemplateFormatVersion:" . cfn-yaml-mode)))
+
+;; Set cfn-lint integration using flycheck if both are installed
+(when (and (featurep 'flycheck)
+           (locate-file "cfn-lint" exec-path exec-suffixes 'file-executable-p))
+  (flycheck-define-checker cfn-lint
+    "AWS CloudFormation linter using cfn-lint.
+
+Install cfn-lint first: pip install cfn-lint
+
+See `https://github.com/aws-cloudformation/cfn-python-lint'."
+
+    :command ("cfn-lint" "-f" "parseable" source)
+    :error-patterns ((warning line-start (file-name) ":" line ":" column
+                              ":" (one-or-more digit) ":" (one-or-more digit) ":"
+                              (id "W" (one-or-more digit)) ":" (message) line-end)
+                     (error line-start (file-name) ":" line ":" column
+                            ":" (one-or-more digit) ":" (one-or-more digit) ":"
+                            (id "E" (one-or-more digit)) ":" (message) line-end))
+    :modes (cfn-json-mode cfn-yaml-mode))
+
+  (add-to-list 'flycheck-checkers 'cfn-lint)
+  (add-hook 'cfn-json-mode-hook 'flycheck-mode)
+  (add-hook 'cfn-yaml-mode-hook 'flycheck-mode))
+
+;;; cfn-mode.el ends here
