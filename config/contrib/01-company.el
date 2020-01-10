21e3f1da39fa4537cdfe02fce74f6461989420aa
Søren Lund <soren@lund.org>
2020-01-10 08:25:49 +0100

    Add company (complete anything) mode

---
+++ config/contrib/01-company.el
@@ -0,0 +1,33 @@
+;;; 01-company.el --- company set up -*-coding:utf-8-*-
+
+;; Copyright (c) 2019 Søren Lund <soren@lund.org>
+
+;; This file is part of dot-emacs.
+
+;; dot-emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; dot-emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with dot-emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Text completion framework. The name stands for "complete anything".
+
+;; See https://company-mode.github.io
+
+;;; Code:
+
+(unless (package-installed-p 'company)
+  (package-install 'company))
+
+(require 'company)
+
+;;; 01-company.el ends here
