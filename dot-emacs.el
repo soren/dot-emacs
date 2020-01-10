7040ad9574104e9a171ae2204df3e9c82cc9242f
Søren Lund <soren@lund.org>
2020-01-10 08:35:05 +0100

    Add creation of lisp directory

--- dot-emacs.el
+++ dot-emacs.el
@@ -135,6 +135,9 @@
     (setq my-diary-file "~/diary")
     (setq my-ispell-change-dictionary "dansk")
 
+    (unless (file-exists-p slu-dot-emacs-my-lisp-dir)
+      (make-directory slu-dot-emacs-my-lisp-dir t))
+
     ;; Load all lisp and configuration files
     (while slu-dot-emacs-config-subdirs
       (setq files (directory-files (concat slu-dot-emacs-config-root-dir "/"
