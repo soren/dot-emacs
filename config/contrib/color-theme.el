36ad3d60d53fe99669c5fb3f2f6e17025da0699d
Søren Lund <soren@lund.org>
2020-01-10 11:39:05 +0100

    Add color-theme

---
+++ config/contrib/color-theme.el
@@ -0,0 +1,33 @@
+;;; color-theme.el --- set up color theme -*-coding:utf-8-*-
+
+;; Copyright (c) 2019 Søren Lund <soren@lund.org>
+
+;; This file is part of dot-emacs.
+
+;; dot-emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; dot-emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with dot-emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Set up dark solarized color theme.
+
+;;; Code:
+
+(if window-system
+    (let ((git-url "https://github.com/sellout/emacs-color-theme-solarized"))      
+      (slu-dot-emacs-clone git-url)
+      (add-to-list 'custom-theme-load-path
+                   (concat slu-dot-emacs-my-lisp-dir (file-name-nondirectory git-url)))
+      (load-theme 'solarized t)))
+
+;;; color-theme.el ends here
