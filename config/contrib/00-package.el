37604a86313f2eea2a02c266fa3a6e3ec9ddb944
Søren Lund <soren@lund.org>
2020-01-12 19:45:03 +0100

    Add elpa package archive

--- config/contrib/00-package.el
+++ config/contrib/00-package.el
@@ -27,6 +27,7 @@
 
 (package-initialize)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 (package-refresh-contents)
