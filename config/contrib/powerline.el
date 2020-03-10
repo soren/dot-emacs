d4c4c57aa3080412e464c4fa62b48564a81a1b84
Søren Lund <soren@lund.org>
2020-03-10 21:35:29 +0100

    Add powerline set up

---
+++ config/contrib/powerline.el
@@ -0,0 +1,32 @@
+;;; powerline.el --- set up powerline -*-coding:utf-8-*-
+
+;; Copyright (c) 2020 Søren Lund <soren@lund.org>
+
+;; This file is part of dot-emacs.
+
+;; dot-emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; dot-emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with dot-emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Set up powerline, default them.
+
+;;; Code:
+
+(if (slu-dot-emacs-clone-and-add
+     "https://github.com/milkypostman/powerline")
+    (progn
+      (require 'powerline)
+      (powerline-default-theme)))
+
+;;; powerline.el ends here
